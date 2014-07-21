bda99e577043056ef007d55b1ce13d7e7baeccfa
Vojtech Krasa <vojta.krasa@gmail.com>
2014-07-21 02:10:27 +0200

    format

--- src/krasa/frameswitcher/CloseProjectsAction.java
+++ src/krasa/frameswitcher/CloseProjectsAction.java
@@ -13,29 +13,28 @@ import java.util.List;
 /**
  * @author Vojtech Krasa
  */
-public class CloseProjectsAction extends DumbAwareAction
-{
+public class CloseProjectsAction extends DumbAwareAction {
 	@Override
 	public void actionPerformed(AnActionEvent anActionEvent) {
-		final CloseProjectsForm form = new CloseProjectsForm(getEventProject(	anActionEvent));
+		final CloseProjectsForm form = new CloseProjectsForm(getEventProject(anActionEvent));
 
-			DialogBuilder builder = new DialogBuilder(getEventProject(	anActionEvent));
-			builder.setCenterPanel(form.getRoot());
-			builder.setDimensionServiceKey("FrameSwitcherCloseProjects");
-			builder.setTitle("Close Projects");
-			builder.removeAllActions();
-			builder.addOkAction();
-			builder.addCancelAction();
+		DialogBuilder builder = new DialogBuilder(getEventProject(anActionEvent));
+		builder.setCenterPanel(form.getRoot());
+		builder.setDimensionServiceKey("FrameSwitcherCloseProjects");
+		builder.setTitle("Close Projects");
+		builder.removeAllActions();
+		builder.addOkAction();
+		builder.addCancelAction();
 
-			boolean isOk = builder.show() == DialogWrapper.OK_EXIT_CODE;
-			if (isOk) {
-				List<Project> checkProjects = form.getCheckProjects();
-				for (Project checkProject : checkProjects) {
-					if (!checkProject.isDisposed()) {
-						ProjectUtil.closeAndDispose(checkProject);
-					}
+		boolean isOk = builder.show() == DialogWrapper.OK_EXIT_CODE;
+		if (isOk) {
+			List<Project> checkProjects = form.getCheckProjects();
+			for (Project checkProject : checkProjects) {
+				if (!checkProject.isDisposed()) {
+					ProjectUtil.closeAndDispose(checkProject);
 				}
-				RecentProjectsManagerBase.getInstance().updateLastProjectPath();
 			}
+			RecentProjectsManagerBase.getInstance().updateLastProjectPath();
+		}
 	}
 }
