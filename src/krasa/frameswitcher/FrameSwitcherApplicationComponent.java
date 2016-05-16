d0959e7e57bcb4d796c350bc37a5b930b91c9115
Vojtech Krasa <vojta.krasa@gmail.com>
2016-05-17 01:49:59 +0200

    IntelliJ 2016 compatibility

--- src/krasa/frameswitcher/FrameSwitcherApplicationComponent.java
+++ src/krasa/frameswitcher/FrameSwitcherApplicationComponent.java
@@ -1,34 +1,29 @@
 package krasa.frameswitcher;
 
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.PathManager;
 import com.intellij.openapi.components.*;
 import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.options.Configurable;
-import com.intellij.openapi.options.ConfigurationException;
 import krasa.frameswitcher.networking.*;
-import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-import javax.swing.*;
 import java.io.File;
 import java.util.UUID;
 
 @State(name = "FrameSwitcherSettings", storages = {@Storage(id = "FrameSwitcherSettings", file = "$APP_CONFIG$/FrameSwitcherSettings.xml")})
 public class FrameSwitcherApplicationComponent implements ApplicationComponent,
-		PersistentStateComponent<FrameSwitcherSettings>, ExportableApplicationComponent, Configurable {
+		PersistentStateComponent<FrameSwitcherSettings>, ExportableApplicationComponent {
 	private final Logger LOG = Logger.getInstance("#" + getClass().getCanonicalName());
 	public static final String IDE_MAX_RECENT_PROJECTS = "ide.max.recent.projects";
 
 
 	private FrameSwitcherSettings settings;
-	private FrameSwitcherGui gui;
 	private RemoteInstancesState remoteInstancesState = new RemoteInstancesState();
 	private RemoteSender remoteSender;
 	private ProjectFocusMonitor projectFocusMonitor;
 
 	public static FrameSwitcherApplicationComponent getInstance() {
-		return ServiceManager.getService(FrameSwitcherApplicationComponent.class);
+		return ApplicationManager.getApplication().getComponent(FrameSwitcherApplicationComponent.class);
 	}
 
 	public FrameSwitcherApplicationComponent() {
@@ -97,60 +92,16 @@ public class FrameSwitcherApplicationComponent implements ApplicationComponent,
 		frameSwitcherSettings.applyMaxRecentProjectsToRegistry();
 	}
 
-	// Configurable---------------------------------
-	@Nls
-	@Override
-	public String getDisplayName() {
-		return "FrameSwitcher";
-	}
-
-	@Nullable
-	public Icon getIcon() {
-		return null;
-	}
-
-	@Nullable
-	@Override
-	public String getHelpTopic() {
-		return null;
-	}
-
-	@Nullable
-	@Override
-	public JComponent createComponent() {
-		if (gui == null) {
-			gui = new FrameSwitcherGui(getState());
-		}
-		return gui.getRoot();
-	}
-
-	@Override
-	public boolean isModified() {
-		return gui.isModified(getState());
-	}
-
-	@Override
-	public void apply() throws ConfigurationException {
-		settings = gui.exportDisplayedSettings();
-		settings.applyOrResetMaxRecentProjectsToRegistry();
-
-		if (remoteSender instanceof DummyRemoteSender && settings.isRemoting()) {
+	public void updateSettings(FrameSwitcherSettings settings) {
+		this.settings = settings;
+		this.settings.applyOrResetMaxRecentProjectsToRegistry();
+		if (remoteSender instanceof DummyRemoteSender && this.settings.isRemoting()) {
 			initRemoting();
-		} else if (remoteSender instanceof RemoteSenderImpl && !settings.isRemoting()) {
+		} else if (remoteSender instanceof RemoteSenderImpl && !this.settings.isRemoting()) {
 			remoteSender.close();
 			remoteInstancesState = new RemoteInstancesState();
 			remoteSender = new DummyRemoteSender();
 		}
-	}
-
-	@Override
-	public void reset() {
-		gui.importFrom(settings);
-	}
-
-	@Override
-	public void disposeUIResources() {
-		gui = null;
 	}
 
 	public RemoteInstancesState getRemoteInstancesState() {
---
+++ src/krasa/frameswitcher/MyConfigurable.java
@@ -0,0 +1,66 @@
+package krasa.frameswitcher;
+
+import com.intellij.openapi.options.Configurable;
+import com.intellij.openapi.options.ConfigurationException;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+public class MyConfigurable implements Configurable {
+	private FrameSwitcherGui gui;
+	@Nls
+	@Override
+	public String getDisplayName() {
+		return "Frame Switcher";
+	}
+
+	@Nullable
+	public Icon getIcon() {
+		return null;
+	}
+
+	@Nullable
+	@Override
+	public String getHelpTopic() {
+		return null;
+	}
+
+	@Nullable
+	@Override
+	public JComponent createComponent() {
+		if (gui == null) {
+			gui = new FrameSwitcherGui(getSettings());
+		}
+		return gui.getRoot();
+	}
+
+	@Override
+	public boolean isModified() {
+		return gui.isModified(getSettings());
+	}
+
+	@Override
+	public void apply() throws ConfigurationException {
+		FrameSwitcherSettings settings = gui.exportDisplayedSettings();
+
+		FrameSwitcherApplicationComponent component = FrameSwitcherApplicationComponent.getInstance();
+		component.updateSettings(settings);
+	}
+
+	@Override
+	public void reset() {
+		gui.importFrom(getSettings());
+	}
+
+	@NotNull
+	private FrameSwitcherSettings getSettings() {
+		return FrameSwitcherApplicationComponent.getInstance().getState();
+	}
+
+	@Override
+	public void disposeUIResources() {
+		gui = null;
+	}
+}
