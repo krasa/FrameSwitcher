558ff2c750ba360e3063025a9b36ffa765dcbddd
Vojtěch Krása <vojta.krasa@gmail.com>
2023-03-30 19:22:54 +0200

    gradle

---
+++ build.gradle.kts
@@ -0,0 +1,63 @@
+plugins {
+    id("java")
+    id("org.jetbrains.intellij") version "1.13.3"
+}
+
+group = "FrameSwitcher"
+version = "4.0.0-212"
+
+tasks {
+    patchPluginXml {
+        sinceBuild.set("212.3117")
+        untilBuild.set("")
+        changeNotes.set(
+            buildString {
+                append("- Fixed console clear button").append("<br>")
+                append("- Supporting regex groups for highlighting")
+            }
+        )
+    }
+
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "11"
+        targetCompatibility = "11"
+    }
+
+
+    signPlugin {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
+
+    publishPlugin {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
+
+    buildSearchableOptions {
+        enabled = false
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+// Configure Gradle IntelliJ Plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
+intellij {
+    version.set("2023.1")
+    type.set("IC") // Target IDE Platform
+
+}
+
+
+dependencies {
+
+    // https://mvnrepository.com/artifact/com.google.guava/guava
+    implementation("com.google.guava:guava:31.1-jre")
+//    https://mvnrepository.com/artifact/org.jgroups/jgroups
+    implementation("org.jgroups:jgroups:5.1.6.Final")
+    implementation("uk.com.robust-it:cloning:1.9.12")
+}
+
